cd252f1000b96eabdc363af18ea7784d69eace3d
victorlmtr <victor.lemaitre1@gmail.com>
2024-07-23 10:04:25 +0200

    /api/continents now working

--- src/main/java/com/gestionrecettes/back/service/ContinentService.java
+++ src/main/java/com/gestionrecettes/back/service/ContinentService.java
@@ -2,22 +2,24 @@ package com.gestionrecettes.back.service;
 
 import com.gestionrecettes.back.model.dto.ContinentDto;
 import com.gestionrecettes.back.model.entity.Continent;
-import com.gestionrecettes.back.repository.ContinentRepository;
 import com.gestionrecettes.back.model.mapper.ContinentMapper;
+import com.gestionrecettes.back.repository.ContinentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ContinentService {
 
-    @Autowired
-    private ContinentRepository continentRepository;
+    private final ContinentRepository continentRepository;
+    private final ContinentMapper continentMapper;
 
     @Autowired
-    private ContinentMapper continentMapper;
+    public ContinentService(ContinentRepository continentRepository, ContinentMapper continentMapper) {
+        this.continentRepository = continentRepository;
+        this.continentMapper = continentMapper;
+    }
 
     public List<ContinentDto> getAllContinents() {
         List<Continent> continents = continentRepository.findAll();
@@ -25,8 +27,9 @@ public class ContinentService {
     }
 
     public ContinentDto getContinentById(Integer id) {
-        Optional<Continent> continent = continentRepository.findById(id);
-        return continent.map(continentMapper::toDto).orElse(null);
+        return continentRepository.findById(id)
+                .map(continentMapper::toDto)
+                .orElse(null);
     }
 
     public ContinentDto createContinent(ContinentDto continentDto) {
@@ -37,17 +40,15 @@ public class ContinentService {
 
     public ContinentDto updateContinent(Integer id, ContinentDto continentDto) {
         if (!continentRepository.existsById(id)) {
-            return null; // Or throw an exception
+            return null;
         }
         Continent continent = continentMapper.toEntity(continentDto);
-        //continent.setId(id);
+        continent.setId(id);
         Continent updatedContinent = continentRepository.save(continent);
         return continentMapper.toDto(updatedContinent);
     }
 
     public void deleteContinent(Integer id) {
-        if (continentRepository.existsById(id)) {
-            continentRepository.deleteById(id);
-        }
+        continentRepository.deleteById(id);
     }
 }
