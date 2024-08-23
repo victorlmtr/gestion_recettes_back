25173001e7cb5a4fe959d599393a42457f00f2d6
victorlmtr <victor.lemaitre1@gmail.com>
2024-08-23 11:52:02 +0200

    added /api/recipes/{id}/ingredients endpoint to fetch all ingredients in a user's pantry

--- src/main/java/com/gestionrecettes/back/repository/StockIngredientRepository.java
+++ src/main/java/com/gestionrecettes/back/repository/StockIngredientRepository.java
@@ -4,6 +4,9 @@ import com.gestionrecettes.back.model.entity.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StockIngredientRepository extends JpaRepository<StockIngredient, StockIngredientId> {
+    List<StockIngredient> findByUtilisateurId(Integer idUtilisateur);
 }
